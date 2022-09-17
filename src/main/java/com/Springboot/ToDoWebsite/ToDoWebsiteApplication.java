4d483cfcdc001e4e056d4627b924c0c2c13e13ef
PranavTiwari44 <tiwa0027@algonquinlive.com>
2022-09-17 19:34:47 -0400

    Started working of ToDoWeb application

---
+++ src/main/java/com/Springboot/ToDoWebsite/ToDoWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.Springboot.ToDoWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  ToDoWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Springboot/ToDoWebsite/ToDoWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Springboot.ToDoWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
